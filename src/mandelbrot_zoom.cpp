188de9e21b18a3121888d026d150db29efa5e1a1
Creepsy <Robin.gd@outlook.de>
2021-04-30 21:54:45 +0200

    rendering of a rectangle

--- src/mandelbrot_zoom.cpp
+++ src/mandelbrot_zoom.cpp
@@ -4,12 +4,24 @@
 #include <cstddef>
 #include <string>
 #include <fstream>
+#include <vector>
 
 #include "shaders.h"
 
 GLFWwindow* create_window(const size_t width, const size_t height, const std::string& title);
+void load_vertices(const std::vector<float>& vertices, unsigned int& vbo, unsigned int& vao);
 
 int main() {
+    const std::vector<float> rect_vertices = {
+        -1.0, -1.0, 0.0,
+        -1.0, 1.0, 0.0,
+        1.0, 1.0, 0.0,
+
+        -1.0, -1.0, 0.0,
+        1.0, 1.0, 0.0,
+        1.0, -1.0, 0.0
+    };
+
     const size_t width = 800, height = 400;
 
     GLFWwindow* window = create_window(width, height, "Mandelbrot Zoom");
@@ -33,16 +45,32 @@ int main() {
         glfwTerminate();
         return -1;
     }
+
+    unsigned int rect_vbo, rect_vao;
+    glGenBuffers(1, &rect_vbo);
+    glGenVertexArrays(1, &rect_vao);
+    load_vertices(rect_vertices, rect_vbo, rect_vao);
+
+    glBindVertexArray(rect_vao);
+
     unsigned int texture_shader = shaders::load_shader_program(texture_vs_shader, texture_fs_shader);
     texture_vs_shader.close();
     texture_fs_shader.close();
 
+    glUseProgram(texture_shader);
+
     while(!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteProgram(texture_shader);
+    glDeleteVertexArrays(1, &rect_vao);
+    glDeleteBuffers(1, &rect_vbo);
 
     glfwTerminate();
 
@@ -59,4 +87,16 @@ GLFWwindow* create_window(const size_t width, const size_t height, const std::st
     GLFWwindow* window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
 
     return window;
+}
+
+void load_vertices(const std::vector<float>& vertices, unsigned int& vbo, unsigned int& vao) {
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(0);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
